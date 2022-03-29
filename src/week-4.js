54a7c1d41608528f929a31df8e8549381b5802af
Thomas <thomas-stalker@live.nmit.ac.nz>
2022-03-30 09:51:44 +1300

    commiting old work

---
+++ src/week-4.js
@@ -0,0 +1,148 @@
+//TODO
+/*
+JavaScript Variables
+*/
+var fullName = "Thomas Stalker" 
+var ClassCode = 503
+
+console.log(fullName)
+console.log(ClassCode)
+
+//  JavaScript Ojects
+var full_name =  
+{
+first_name:"Thomas",
+last_name:"Stalker",
+course_number:503
+}
+
+// console.log(full_name)
+// console.log(full_name.first_name)
+// console.log(full_name["last_name"])
+
+// //  JavaScript Functions
+// function addTwoNumbers(num1,num2) {
+// return num1+num2
+// }
+// console.log(addTwoNumbers(12,3))
+
+// function addThreeNumbers(num1,num2,num3) { 
+// return num1+num2+num3
+// }
+// console.log(addThreeNumbers(4,7,1,))
+
+// //  JavaScript Loops
+// // for(var i=0; i< 10; i ++){
+// //     console.log(1)
+// // }
+
+//  JavaScript if statements
+var number = 10
+if (number === 10) {
+    console.log("answer = " + number )
+} else {
+    console.log("error")
+}
+
+//  const and let
+let num = 6
+console.log(num)
+
+num = "Thomas"
+console.log(num)
+
+//  JavaScript comments
+ 
+
+/**
+ Hello World
+ class SDV503
+ */
+
+var sdv503 = "javascript"
+var x = 6
+var fullName = "text"
+
+//foo(x,y) 
+function foo (p1, p2) {
+return p2 - p1
+}
+
+function sum (x1, x2) {
+return x1 + x2
+}
+
+sum (10,3)
+
+// (=) v= (===) 
+//statements vs expressions
+var numbers
+//statement
+// var x
+// if (y >= 0) {
+//     x = y
+// } else {
+//     x = -y
+// }
+
+// function statement(x) {
+//     if(y >= 0)
+//     return x = y
+// } else {
+//     return x = -y
+// }
+
+// //expression ES6
+// var x = y >= 0 ? y : -y
+
+// var word = "Thomas"
+// console.log(word.length)
+
+var obj = {}
+// console.log(obj)
+
+// obj.foo = 123
+// console.log(obj)
+// console.log(word.toUpperCase())
+
+//JavaScript va;ies (primitives vs objects)
+
+var obj1 = {}
+var obj2 = {}
+
+console.log(obj1 === obj1)
+
+var number_one = 123
+var number_two = 123
+
+
+// console/log(number_one === number_two)
+
+//Boolean: true and false
+//Numbers: 1234, 12432.4
+//strings: 'asdfg;, "asdfg"
+//non values: null and undefined
+
+// var str = "stalker"
+// console.log(str.length = 2)
+// console.log(str.length)
+// 4
+// var numbers = [1, 2, 3, 4, 5]
+// console.log(numbers[3] =10)
+// console.log(numbers)
+
+// ({} === {})
+
+// var nonvalue = {}
+
+//function nonvalue(x) {
+// return x
+// }
+
+var value = {}
+typeof value
+//console.log(nonvalue.first_name)
+
+//if (value === undefomed || value === null)
+
+//false, 0, '', NaN
